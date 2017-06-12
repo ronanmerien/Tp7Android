758d4cde1cd662b6ea01421192c83d96578730c9
Ronan Merien <ronan.merien@gmail.com>
2017-06-12 16:00:29 +0200

    infinite loop

--- app/src/main/java/com/example/admin/tp7android/ResponseActivity.java
+++ app/src/main/java/com/example/admin/tp7android/ResponseActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.tp7android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResponseActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class ResponseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("param");
+        TextView editText = (TextView) findViewById(R.id.textView2);
+        editText.setText(text);
+    }
+
+    public void onClickrepa(View view) {
+        Intent intent = new Intent(ResponseActivity.this,MainActivity.class);
+        intent.putExtra("param", "Je viens du bouton1");
+        startActivity(intent);
+    }
+
+    public void onClickrepb(View view) {
+        Intent intent = new Intent(ResponseActivity.this,MainActivity.class);
+        intent.putExtra("param", "Je viens du bouton2");
+        startActivity(intent);
     }
 }
