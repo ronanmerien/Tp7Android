a85ab7c40956d0ac8cf8c027c76138da297fd247
Ronan Merien <ronan.merien@gmail.com>
2017-06-12 15:31:48 +0200

    new activity

--- app/src/main/java/com/example/admin/tp7android/MainActivity.java
+++ app/src/main/java/com/example/admin/tp7android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.admin.tp7android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ResponseActivity.class);
+                intent.putExtra("param", "Je viens du bouton2");
+                startActivity(intent);
+            }
+        });
     }
 }
